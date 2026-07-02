907d6ab2f67b04c2702b0f7bfbd10c49c769b340
Modou Ndiaye <modoundiaye@Modous-MacBook-Pro.local>
2026-07-01 20:21:47 -0400

    fix payment consumer

--- src/main/java/com/smartcity/payment/consumer/PaymentConsumer.java
+++ src/main/java/com/smartcity/payment/consumer/PaymentConsumer.java
@@ -5,21 +5,39 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Slf4j
 @Service
 public class PaymentConsumer {
 
-    @KafkaListener(topics = "parking-session-ended", groupId = "payment-group")
+    @KafkaListener(topics = "parking-session-ended", groupId = "payment-group-v2")
     public void consume(ParkingSessionEndedEvent event) {
 
         log.info("Event reçu: {}", event);
 
         // Calcul simple
-        long duration = java.time.Duration
+        /*long duration = java.time.Duration
                 .between(event.getStartTime(), event.getEndTime())
                 .toMinutes();
 
-        double price = duration * 0.05;
+        double price = duration * 0.05;*/
+
+        Duration duration =
+                Duration.between(event.getStartTime(), event.getEndTime());
+
+        long seconds = duration.getSeconds();
+
+        long billableMinutes =
+                (long) Math.ceil(seconds / 60.0);
+
+        double price = billableMinutes * 0.05;
+
+        log.info("Durée réelle : {} secondes", seconds);
+
+        log.info("Minutes facturées : {}", billableMinutes);
+
+        log.info("Montant : {} CAD", price);
 
         log.info("Paiement calculé pour {} : {} CAD", event.getPlate(), price);
     }
